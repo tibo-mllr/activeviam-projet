87f8dffb13a9861c4f6d1b2a57044f985cb5b747
AlexandreCGithub <alexandre.correia2@edu.devinci.fr>
2024-11-22 02:13:58 +0100

    fix: fixing display not in english

--- app/submitQuery/page.tsx
+++ app/submitQuery/page.tsx
@@ -93,7 +93,7 @@ export default function SubmitQueryPage(): ReactElement {
                 type="submit"
                 className="w-full py-2 px-4 bg-blue-600 text-white font-medium rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 dark:bg-blue-500 dark:hover:bg-blue-600"
               >
-                Envoyer
+                Send
               </button>
             </Form>
           )}
