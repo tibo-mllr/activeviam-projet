a94f99d99ce09845c344cb9408f7c85ad8c5d22c
AlexandreCGithub <alexandre.correia2@edu.devinci.fr>
2024-12-08 00:37:18 +0100

    fix: first test

--- app/summary/page.tsx
+++ app/summary/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { getQueryPlan } from "@/lib/redux";
-import { Typography, Card, CardContent } from "@mui/material";
+import { Card } from "@mui/material";
 import { ReactElement } from "react";
 import { useSelector } from "react-redux";
 
 export default function SummaryPage(): ReactElement {
   const queryPlan = useSelector(getQueryPlan);
-
+  console.log(queryPlan);
   return <Card></Card>;
 }
