69459ef8d8357b6f099463347255b874db125c64
AlexandreCGithub <alexandre.correia2@edu.devinci.fr>
2024-12-08 01:03:14 +0100

    feat: first display of infos in summary page

--- app/summary/page.tsx
+++ app/summary/page.tsx
@@ -1,12 +1,52 @@
 "use client";
 
 import { getQueryPlan } from "@/lib/redux";
-import { Card } from "@mui/material";
+import { Card, CardContent, Grid2, Typography } from "@mui/material";
 import { ReactElement } from "react";
 import { useSelector } from "react-redux";
 
 export default function SummaryPage(): ReactElement {
   const queryPlan = useSelector(getQueryPlan);
-  console.log(queryPlan);
-  return <Card></Card>;
+
+  if (queryPlan !== "") {
+    // If we have a queryPlan loaded
+    const totalAggregateRetrievals = queryPlan.reduce((total, qP) => {
+      return total + qP.aggregateRetrievals.length;
+    }, 0);
+
+    const totalDatabaseRetrievals = queryPlan.reduce((total, qP) => {
+      return total + qP.databaseRetrievals.length;
+    }, 0);
+
+    return (
+      <Card>
+        <CardContent>
+          <Grid2 container spacing={2}>
+            <Grid2>
+              <Typography variant="h6">Summary</Typography>
+            </Grid2>
+            <Grid2>
+              <Typography variant="body1">
+                Total Aggregate Retrievals: {totalAggregateRetrievals}
+              </Typography>
+            </Grid2>
+            <Grid2>
+              <Typography variant="body1">
+                Total Database Retrievals: {totalDatabaseRetrievals}
+              </Typography>
+            </Grid2>
+          </Grid2>
+        </CardContent>
+      </Card>
+    );
+  }
+
+  // Default display
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant="body1">No data available</Typography>
+      </CardContent>
+    </Card>
+  );
 }
