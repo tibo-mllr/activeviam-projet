633cc2db93b8807c04ecbf330ea8b33295ca6fa4
AlexandreCGithub <alexandre.correia2@edu.devinci.fr>
2024-12-09 20:40:27 +0100

    feat: adding aggregateRetrievals details on summary page

--- app/summary/page.tsx
+++ app/summary/page.tsx
@@ -1,22 +1,39 @@
 "use client";
 
 import { getQueryPlan } from "@/lib/redux";
-import { Card, CardContent, Grid2, Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Grid2,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
 import { ReactElement } from "react";
 import { useSelector } from "react-redux";
 
 export default function SummaryPage(): ReactElement {
   const queryPlan = useSelector(getQueryPlan);
+  console.log(queryPlan);
 
   if (queryPlan !== "") {
     // If we have a queryPlan loaded
-    const totalAggregateRetrievals = queryPlan.reduce((total, qP) => {
-      return total + qP.aggregateRetrievals.length;
-    }, 0);
+    const [SelectPass_0] = queryPlan;
+    const totalAggregateRetrievals = SelectPass_0.aggregateRetrievals.length;
+    const totalDatabaseRetrievals = SelectPass_0.databaseRetrievals.length;
 
-    const totalDatabaseRetrievals = queryPlan.reduce((total, qP) => {
-      return total + qP.databaseRetrievals.length;
-    }, 0);
+    const counts: Record<string, number> = {};
+
+    SelectPass_0.aggregateRetrievals.forEach((item) => {
+      const type_name = item.type;
+      if (!counts[type_name]) {
+        counts[type_name] = 0;
+      }
+      counts[type_name]++;
+    });
+
+    console.log("Counts:", counts);
 
     return (
       <Card>
@@ -29,7 +46,18 @@ export default function SummaryPage(): ReactElement {
               <Typography variant="body1">
                 Total Aggregate Retrievals: {totalAggregateRetrievals}
               </Typography>
+              <List dense sx={{ marginLeft: 4 }}>
+                {Object.entries(counts).map(([type, count]) => (
+                  <ListItem key={type} disablePadding>
+                    <ListItemText primary={`${type}: ${count}`} />
+                  </ListItem>
+                ))}
+              </List>
             </Grid2>
+          </Grid2>
+        </CardContent>
+        <CardContent>
+          <Grid2 container spacing={2}>
             <Grid2>
               <Typography variant="body1">
                 Total Database Retrievals: {totalDatabaseRetrievals}
