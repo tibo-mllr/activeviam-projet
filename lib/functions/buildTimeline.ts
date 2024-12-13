610d2e093d0de315ff7776057ec4b955ac14b8cd
Thibault Muller <thibault.muller19@gmail.com>
2024-12-13 15:05:27 +0100

    fix: timeline computation

--- lib/functions/buildTimeline.ts
+++ lib/functions/buildTimeline.ts
@@ -2,47 +2,61 @@ import { AggregateRetrieval } from "../types";
 
 export function buildTimeline(
   retrievals: AggregateRetrieval[],
-): Record<string, [number, number][]> {
-  const res: Record<number, [number, number][]> = {};
+): Record<number, [number, number][]> {
+  const allTimings: [number, number][] = [];
 
   for (const retrieval of retrievals) {
     const { timingInfo } = retrieval;
     Object.entries(timingInfo).map(([key, values]) => {
       if (key === "startTime") {
-        values.map((value, core) => {
-          const elapsedTime = timingInfo["elapsedTime"][core];
-          if (elapsedTime !== 0) {
-            if (!res[core]) res[core] = [];
-
-            res[core].push([value, value + elapsedTime]);
-          }
+        values.map((value, index) => {
+          const elapsedTime = timingInfo["elapsedTime"][index];
+          if (elapsedTime !== 0) allTimings.push([value, value + elapsedTime]);
         });
       } else if (key === "executionContextStartTime") {
-        values.map((value, core) => {
-          const elapsedTime = timingInfo["executionContextElapsedTime"][core];
-          if (elapsedTime !== 0) {
-            if (!res[core]) res[core] = [];
-
-            res[core].push([value, value + elapsedTime]);
-          }
+        values.map((value, index) => {
+          const elapsedTime = timingInfo["executionContextElapsedTime"][index];
+          if (elapsedTime !== 0) allTimings.push([value, value + elapsedTime]);
         });
       }
     });
   }
 
-  // Remove duplicates
-  for (const core in res) {
-    res[core] = res[core].reduce(
-      (acc, curr) => {
-        if (!acc.find(([start, end]) => start === curr[0] && end === curr[1])) {
-          acc.push(curr);
-        }
+  // Remove duplicates for easier grouping
+  const filteredTimings = allTimings.reduce(
+    (acc, curr) => {
+      if (!acc.find(([start, end]) => start === curr[0] && end === curr[1])) {
+        acc.push(curr);
+      }
 
-        return acc;
-      },
-      [] as [number, number][],
-    );
+      return acc;
+    },
+    [] as [number, number][],
+  );
+
+  // Sort by start time
+  filteredTimings.sort((a, b) => a[0] - b[0]);
+
+  const timeline: Record<number, [number, number][]> = {};
+
+  // Group by core, by trying to fit time intervals into the first available core
+  for (const timing of filteredTimings) {
+    const [start] = timing;
+    let core = 0;
+    while (true) {
+      if (core in timeline) {
+        const lastTiming = timeline[core][timeline[core].length - 1];
+        if (start >= lastTiming[1]) {
+          timeline[core].push(timing);
+          break;
+        }
+      } else {
+        timeline[core] = [timing];
+        break;
+      }
+      core++;
+    }
   }
 
-  return res;
+  return timeline;
 }
